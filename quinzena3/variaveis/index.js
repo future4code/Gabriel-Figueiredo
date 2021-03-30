d330ebd1fdfff2da4f9310130bf625440ee42879
GabrielCFigueiredo <gabriel.devsistema@gmail.com>
2021-03-30 20:47:11 -0300

    foi adicionado alguns codigos js.

---
+++ quinzena3/variaveis/index.js
@@ -0,0 +1,98 @@
+
+/* 1. a = 10
+    b= 10
+    console.log(b) será impresso o valor de b=10
+    b = 5
+    console.log(b) será impresso o novo valor de b= 5 */ 
+
+/* a = 10
+    b = 20
+    c = a
+    b = c
+    a = b 
+
+console.log(a, b, c) será impresso tanto em a, b, c o valor 10 */
+
+
+    /* let nomeUsuario              resposta undefined    
+    typeof nomeUsuario
+
+    let idadeUsuario                resposta undefined
+    typeof idadeUsuario */
+
+//d- Foi impresso indefined porque não foi declarado o valor da variavel
+
+
+ //let nomeUsuario = prompt ("qual o nome do Usuario ?") 
+ //let idadeUsuario = prompt ("qual sua idade ?")
+
+ //e - eu notei que tanto quando perguntamos o nome ou a idade o tipo da variavel é uma String
+
+ /* f- const nome = "Gabriel"
+ const idade = "35"
+
+ console.log("Olá",nome, "você tem",idade, "anos.")*/
+
+ /* 2 - let time = "corinthians"
+ let cor = "branco"
+ let comida = "churrasco"
+ let musica = "sertanejo"
+ let lazer = "piscina"
+
+
+ console.log("1. Qual é seu time de coração?")
+ console.log("resposta:", time)
+
+ console.log("2. Qual é a sua cor preferida?")
+ console.log("resposta:", cor)
+
+ console.log("3. Qual é a comida preferida?")
+ console.log("resposta:",comida )
+
+ console.log("4. Qual é seu estilo musical?")
+ console.log("resposta:", musica)
+
+ console.log("5. O lazer preferido da sua familia ?")
+ console.log("resposta:", lazer)*/
+
+ /* 3 - let comida = ["pizza", "lasanha", "parmegiana", "strogonoff", "lanche"]
+
+ console.log(comida)
+
+ console.log("Essas são as minhas comidas preferidas:")
+
+ console.log(comida[0])
+ console.log(comida[1])
+ console.log(comida[2])
+ console.log(comida[3])
+ console.log(comida[4])
+
+ console.log("Qual sua comida preferida:")
+
+ comida[1] = "batata"
+
+ console.log(comida[0])
+ console.log(comida[1])
+ console.log(comida[2])
+ console.log(comida[3])
+ console.log(comida[4]) */
+
+ let diaHoje = ["true", "false"]
+
+ console.log("Hoje é sabado?")
+ console.log(diaHoje[1]);
+
+ console.log("Hoje esta ensolarado?")
+ console.log(diaHoje[0]);
+
+ console.log("Estamos no ano de 2020?")
+ console.log(diaHoje[1]);
+
+
+
+
+ 
+
+
+
+ 
