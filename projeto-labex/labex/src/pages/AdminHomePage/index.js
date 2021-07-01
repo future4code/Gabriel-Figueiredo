8e72a833552e773d9484589ac6dd40ec98adbb63
Gabriel <gabriel.devsistema@gmail.com>
2021-07-01 09:44:20 -0300

    atualizar

--- projeto-labex/labex/src/pages/AdminHomePage/index.js
+++ projeto-labex/labex/src/pages/AdminHomePage/index.js
@@ -1,7 +1,26 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 import { useHistory } from "react-router-dom";
 
 function AdminHomePage() {
+
+  const [detail, setDetailPage] = useState("")
+
+  useEffect(() => {
+    const token = localStorage.getItem("token")
+    axios.get("https://us-central1-labenu-apis.cloudfunctions.net/labeX/gabriel/trip/Z8NOkDl2y02uZc2Zc8jC", {
+      headers: {
+        auth: token
+      }
+    })
+    .then((response) => {
+      console.log("Logado", response.data.trip);
+      setDetailPage(response.data.trip)
+    })
+    .catch((error) => {
+      console.log("não logado", error.response);
+    })
+  },[])
 
   const history = useHistory()
 
@@ -19,6 +38,7 @@ function AdminHomePage() {
   return (
     <div>
      <h1>AdminHomePage</h1>
+     <p>Nome : {detail.name}</p>
      <button onClick={pageHome}>Home</button>
      <button onClick={tripDetails}>Trip Details</button>
      <button onClick={createTrip}>Create trip</button>
--- projeto-labex/labex/src/pages/TripDetailsPage/index.js
+++ projeto-labex/labex/src/pages/TripDetailsPage/index.js
@@ -1,34 +1,21 @@
-import axios from "axios";
-import React, { useEffect, useState } from "react";
+
+import React from "react";
 import { useHistory } from "react-router-dom";
 
 function TripDetailsPage() {
 
-  const [detail, setDetailPage] = useState("")
+  
   const history = useHistory()
 
   const pageHome = () => {
     history.goBack("/")
   }
   
-  useEffect(() => {
-    const token = localStorage.getItem("token")
-    axios.get("https://us-central1-labenu-apis.cloudfunctions.net/labeX/gabriel/trip/Z8NOkDl2y02uZc2Zc8jC", {
-      headers: {
-        auth: token
-      }
-    })
-    .then((response) => {
-      console.log("Logado", response.data.trip);
-      setDetailPage(response.data.trip)
-    })
-    .catch((error) => {
-      console.log("não logado", error.response);
-    })
-  },[])
+ 
   return (
     <div>
      <h1>TripDetailsPage</h1>
+     
      <button onClick={pageHome}>Home</button>
     </div>
   );
